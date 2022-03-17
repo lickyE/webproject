7f5fe941404a9cf1ad4463eca67ddba6167fec4b
huzongyu <619873031@qq.com>
2022-03-17 13:48:35 +0800

    22_3_17_1_version

--- src/main/java/com/nowcoder/community/community/service/CommentService.java
+++ src/main/java/com/nowcoder/community/community/service/CommentService.java
@@ -2,16 +2,28 @@ package com.nowcoder.community.community.service;
 
 import com.nowcoder.community.community.dao.CommentMapper;
 import com.nowcoder.community.community.entity.Comment;
+import com.nowcoder.community.community.util.CommunityConstent;
+import com.nowcoder.community.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstent {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
 
     public List<Comment> findCommentByEntity(int entityType,int entityId,int offset,int limit){
         return commentMapper.selectCommentByEntity(entityType,entityId,offset,limit);
@@ -19,5 +31,21 @@ public class CommentService {
 
     public int findCommentCount(int entityType,int entityId){
         return commentMapper.selectCountByEntity(entityType,entityId);
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED,propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment){
+        if(comment == null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        if(comment.getEntityType() == ENTITY_TYPE_POST){
+            int replyCount = commentMapper.selectCountByEntity(ENTITY_TYPE_POST,comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(),replyCount);
+        }
+        return rows;
     }
 }
